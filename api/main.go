d3a1b3601a5ad02dcf31cd25921b345f4d9b5800
ags4659 <ags4659@gmail.com>
2023-05-02 23:48:24 +1000

    We implemented New Sport Service which will list sports events

--- api/main.go
+++ api/main.go
@@ -1,100 +1,60 @@
 package main
 
 import (
-	"context"
-	"encoding/json"
-	"flag"
-	"log"
-	"net/http"
-	"strconv"
+    "context"
+    "flag"
+    "log"
+    "net/http"
 
-	"github.com/gorilla/mux"
-	"google.golang.org/grpc"
-
-	"git.neds.sh/matty/entain/api/proto/racing"
+    "git.neds.sh/matty/entain/api/proto/racing"
+    "git.neds.sh/matty/entain/api/proto/sports"
+    "github.com/grpc-ecosystem/grpc-gateway/runtime"
+    "google.golang.org/grpc"
+   
 )
 
 var (
-	apiEndpoint  = flag.String("api-endpoint", "localhost:8000", "API endpoint")
-	grpcEndpoint = flag.String("grpc-endpoint", "localhost:9000", "gRPC server endpoint")
+    apiEndpoint  = flag.String("api-endpoint", "localhost:8000", "API endpoint")
+    grpcEndpoint = flag.String("grpc-endpoint", "localhost:9000", "gRPC server endpoint")
+    sportsEndpoint = flag.String("sports-endpoint", "localhost:7000", "Sports gRPC server endpoint")
 )
 
 func main() {
-	flag.Parse()
+    flag.Parse()
 
-	if err := run(); err != nil {
-		log.Printf("failed running api server: %s\n", err)
-	}
+    if err := run(); err != nil {
+        log.Printf("failed running api server: %s\n", err)
+    }
 }
 
 func run() error {
-	ctx := context.Background()
-	ctx, cancel := context.WithCancel(ctx)
-	defer cancel()
+    ctx := context.Background()
+    ctx, cancel := context.WithCancel(ctx)
+    defer cancel()
 
-	// Create a new Gorilla Mux router
-	r := mux.NewRouter()
+    mux := runtime.NewServeMux()
 
-	// Create a new gRPC client connection
-	conn, err := grpc.Dial(*grpcEndpoint, grpc.WithInsecure())
-	if err != nil {
-		return err
-	}
-	defer conn.Close()
+    // Register the Racing gRPC service
+    if err := racing.RegisterRacingHandlerFromEndpoint(
+        ctx,
+        mux,
+        *grpcEndpoint,
+        []grpc.DialOption{grpc.WithInsecure()},
+    ); err != nil {
+        return err
+    }
 
-	// Create a new RacingClient
-	client := racing.NewRacingClient(conn)
+    // Register the Sports gRPC service
+    if err := sports.RegisterSportsHandlerFromEndpoint(
+        ctx,
+        mux,
+        *sportsEndpoint,
+        []grpc.DialOption{grpc.WithInsecure()},
+    ); err != nil {
+        return err
+    }
 
-	// Define the routes for each method
-	listRacesRoute := "/v1/list-races"
-	getRaceRoute := "/v1/get-race/{id}"
+    log.Printf("API server listening on: %s\n", *apiEndpoint)
 
-	// Register the ListRaces route handler
-	r.HandleFunc(listRacesRoute, func(w http.ResponseWriter, r *http.Request) {
-		req := &racing.ListRacesRequest{}
-
-		resp, err := client.ListRaces(r.Context(), req)
-		if err != nil {
-			log.Printf("failed to handle ListRaces request: %s", err)
-			w.WriteHeader(http.StatusInternalServerError)
-			return
-		}
-
-		w.Header().Set("Content-Type", "application/json")
-		w.WriteHeader(http.StatusOK)
-		json.NewEncoder(w).Encode(resp)
-	}).Methods(http.MethodGet)
-
-	// Register the GetRace route handler
-	r.HandleFunc(getRaceRoute, func(w http.ResponseWriter, r *http.Request) {
-		vars := mux.Vars(r)
-		id := vars["id"]
-
-				// Convert id to int64
-		idInt, err := strconv.ParseInt(id, 10, 64)
-		if err != nil {
-			log.Printf("failed to parse id: %s", err)
-			w.WriteHeader(http.StatusBadRequest)
-			return
-		}
-
-		// Create a new GetRaceRequest with the ID provided in the URL path
-		req := &racing.GetRaceRequest{Id: idInt}
-
-
-		resp, err := client.GetRace(r.Context(), req)
-		if err != nil {
-			log.Printf("failed to handle GetRace request: %s", err)
-			w.WriteHeader(http.StatusInternalServerError)
-			return
-		}
-
-		w.Header().Set("Content-Type", "application/json")
-		w.WriteHeader(http.StatusOK)
-		json.NewEncoder(w).Encode(resp)
-	}).Methods(http.MethodGet)
-
-	log.Printf("API server listening on: %s\n", *apiEndpoint)
-
-	return http.ListenAndServe(*apiEndpoint, r)
+    return http.ListenAndServe(*apiEndpoint, mux)
 }
